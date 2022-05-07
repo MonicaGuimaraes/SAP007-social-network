d08df17acc443fe0473ea1f584aba266d2ae1f2a
AnnaIsah <annaisabellef@gmail.com>
2022-05-07 20:18:00 -0300

    test: like function

--- test/pages/feed.spec.js
+++ test/pages/feed.spec.js
@@ -2,7 +2,9 @@
 * @jest-environment jsdom
 */
 import { feed } from '../../src/pages/feed/feed.js';
-import { createPost, getAllPosts, authLogOut } from '../../src/pages/feed/firestore-functions.js';
+import {
+  createPost, getAllPosts, authLogOut, likePost, removeLikePost,
+} from '../../src/pages/feed/firestore-functions.js';
 import {
   postElement,
 } from '../../src/components/timelinepost.js';
@@ -16,6 +18,45 @@ const user = {
   displayName: '',
   uid: '',
 };
+
+const user1 = {
+  photoURL: '',
+  displayName: 'Nome',
+  uid: 'uid',
+};
+
+const posts = [
+  {
+    id: 'beleza',
+    data() {
+      return {
+        message: 'testando',
+        edit: '',
+        userUid: user1.uid,
+        name: user1.displayName,
+        imgProfile: user1.photoURL,
+        like: [],
+        comment: [],
+        day: { second: new Date().getTime() / 1000 },
+      };
+    },
+  },
+  {
+    id: 'oi',
+    data() {
+      return {
+        message: 'testando',
+        edit: '',
+        userUid: user.uid,
+        name: user.displayName,
+        imgProfile: user.photoURL,
+        like: [user1.uid],
+        comment: [],
+        day: { second: new Date().getTime() / 1000 },
+      };
+    },
+  },
+];
 
 describe('feed e getAllPosts', () => {
   it('feed(user) deve ser um Node.ELEMENT_NODE (1) e getAllPosts deve ser chamada', () => {
@@ -71,39 +112,6 @@ describe('createPost(post)', () => {
 });
 
 describe('postElement() e getAllPosts', () => {
-  const posts = [
-    {
-      id: 'beleza',
-      data() {
-        return {
-          message: 'testando',
-          edit: '',
-          userUid: user.uid,
-          name: user.displayName,
-          imgProfile: user.photoURL,
-          like: [],
-          comment: [],
-          day: { second: new Date().getTime() / 1000 },
-        };
-      },
-    },
-    {
-      id: 'oi',
-      data() {
-        return {
-          message: 'testando',
-          edit: '',
-          userUid: user.uid,
-          name: user.displayName,
-          imgProfile: user.photoURL,
-          like: ['a'],
-          comment: [],
-          day: { second: new Date().getTime() / 1000 },
-        };
-      },
-    },
-  ];
-
   it('postElement(post, user, id) deve retornar uma div ou p Node.ELEMENT_NODE (1)', async () => {
     await getAllPosts.mockResolvedValueOnce();
     expect(postElement(posts[1].data(), user, await createPost.mockReturnValue({ id: 'oi' })).nodeType).toBe(1);
@@ -127,5 +135,30 @@ describe('AuthLogOut', () => {
     const btnLogOut = timeline.querySelector('#btn-log-out');
     btnLogOut.dispatchEvent(new Event('click'));
     expect(authLogOut).toHaveBeenCalled();
+  });
+});
+
+describe('likePost', () => {
+  it('Deverá trazer um like a publicação', async () => {
+    const timelinePost = postElement(posts[0].data(), user, posts[0].id);
+    const btnLike = timelinePost.querySelector('.post-like');
+    const valueLike = timelinePost.querySelector('.value-like');
+    likePost.mockResolvedValueOnce();
+    btnLike.dispatchEvent(new Event('click'));
+    await new Promise(process.nextTick);
+    expect(likePost).toHaveBeenCalled();
+    expect(valueLike.textContent).toBe('1');
+  });
+});
+describe('removeLikePost', () => {
+  it('Deverá remover um like da publicação', async () => {
+    await new Promise(process.nextTick);
+    const timelinePost = postElement(posts[1].data(), user1, posts[1].id);
+    const btnLike = timelinePost.querySelector('.post-like');
+    const valueLike = timelinePost.querySelector('.value-like');
+    removeLikePost.mockResolvedValueOnce();
+    btnLike.dispatchEvent(new Event('click'));
+    expect(removeLikePost).toHaveBeenCalledTimes(1);
+    expect(valueLike.textContent).toBe('1');
   });
 });
