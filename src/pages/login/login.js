9edede0f8ede8dc695366f9fafe6dfb6e0355fa3
Voskrov <monicapeixoto0@gmail.com>
2022-05-08 23:53:53 -0300

    README ajuste

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -19,8 +19,8 @@ export const pageLogin = () => {
       <form method="post" class = "form-login flex-direction">
         <input type="email" placeholder="seu@email.com" class="login-area font-size" id="email-area" name="email-area" requered>
         <input type="password" placeholder="Senha" class="login-area font-size" id="password-area" name="password-area" requered>
-        <button class="btn-recover-password font-size" id="btn-recover-password">Esqueci minha senha</button>         
-        <button class="btn-sign-in font-size" id="btn-sign-in">Entrar</button>          
+        <button class="btn-sign-in font-size" id="btn-sign-in">Entrar</button>
+        <button class="btn-recover-password font-size" id="btn-recover-password">Esqueci minha senha</button>                
       </form>
       <p class="error text-center font-size" id="user-error"></p> 
       <p class="error text-center font-size" id = "user-error-gmail"></p>
